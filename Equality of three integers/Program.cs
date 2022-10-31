e1acd0decc7f441d4b36a35c5bf88ee580f1a7e1
Rintu <Rintu@VINDUJA>
2022-10-31 16:36:26 +0530

    Add project files.

---
+++ Equality of three integers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equality_of_three_integers
+{
+    internal class Program
+    {
+        // Here we can use if condition also to compare values, but here used ternary operator
+        static void Main(string[] args)
+        {
+            int x = 1, y = 2, z = 3;
+            Console.WriteLine("Original numbers: " + x + ',' + y + ',' + z);
+            Console.WriteLine("Number of equality of the said numbers: " + test(x, y, z));
+            x = 1; y = 3; z = 3;
+            Console.WriteLine("Original numbers: " + x + ',' + y + ',' + z);
+            Console.WriteLine("Number of equality of the said numbers: " + test(x, y, z));
+            x = 3; y = 3; z = 3;
+            Console.WriteLine("Original numbers: " + x + ',' + y + ',' + z);
+            Console.WriteLine("Number of equality of the said numbers: " + test(x, y, z));
+            Console.ReadLine();
+        }
+        public static int test(int x, int y, int z)
+        {
+            return (x == y && y == z) ? 3 : (x != y && y != z && x != z) ? 0 : 2;
+        }
+    }
+}
---
+++ Sorting characters in given string/Sorting_characters.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_characters_in_given_string
+{
+    internal class Sorting_characters
+    {
+       static void Main(string[] args)
+        {
+             string inputs = "bdfcasx";
+            Console.WriteLine(sortString(inputs));
+        }
+        //public static string test(string text)
+        //{
+        //    bool flag=string.IsNullOrEmpty(text);
+        //    if (flag)
+        //        return "String is Blank";
+
+        //    var text_nums=text.Where(char.IsDigit).OrderBy(e1 => e1).ToList();
+        //    var text_chars = text.Where(char.IsLetter)
+        //        .Select(e1 => new { 1_char = char.ToLower(e1), _char => e1 })
+        //        .OrderByDescending(e1 => e1._char)
+        //        .ToList();
+
+        //    return new string(text_chars.Select(e1 => e1._char).Concat(text_nums).ToArray());
+        //}
+
+
+
+        public static string sortString(string inputs)
+        {
+            char[] temp=inputs.ToCharArray();
+            Array.Sort(temp);
+            return new string(temp);
+        }
+    }
+}
